ac25f9e3815965309992133926fe582cb1080fbd
Eduardo Rosua <eduardorosuae@gmail.com>
2021-04-06 09:35:08 +0200

    Creación de componente para 404

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,12 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { HomeComponent } from "./components/home.component";
+import { ErrorComponent } from "./components/error.component";
 
 const appRoutes:Routes = [
     {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
-    {path: '**', component: HomeComponent} // Debe ser siempre la última
+    {path: '**', component: ErrorComponent} // Debe ser siempre la última
 
 ];
 
